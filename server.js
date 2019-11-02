4ddc5a9fba42a3f132d1fb656ac8189cb8f3401a
vssobol <vssobol@gmail.com>
2019-11-02 12:59:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,33 +7,33 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 
-app.get('/', (request, response) => {
+app.get('/', (req, res) => {
   response.send('Home Page!');
 });
 
-app.get('/bad', (request, response) => {
+app.get('/bad', (req, res) => {
   throw new Error('Not Found');
 })
 
 app.get('/about', aboutUsHandler);
 
-function aboutUsHandler(request, response){
+function aboutUsHandler(req, res){
   response.status(200).send('This is the about page .html');
 }
 
-app.get('*', (request,response) => {
+app.get('*', (req, res) => {
   response.status(404).send('Route does not exist');
 })
 
-app.get('/location', (request, response) => {
+app.get('/location', (req, res) => {
   try{
     const geoData = require('./data/geo.json');
-    const city = request.query.data;
+    const city = req.query.data;
     const locationData = new Location(city, geoData);
     response.send(locationData);
   }
   catch(error){
-    errorHandler('Something went wrong', request, response);
+    errorHandler('Something went wrong', req, res);
   }
 })
 
