20032ce1471a9ef1b5b2104d2d580a2932ff3b6e
hendricendrix <hendricentrix@gmail.com>
2019-11-02 13:51:35 -0700

    b2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 
